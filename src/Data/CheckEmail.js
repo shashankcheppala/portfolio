a2229826ed6d6f4d52492eabd525f0353a3e5c0b
Shashank Cheppala <notedevelopment@gmail.com>
2025-09-10 22:07:01 -0500

    yes

--- src/Data/CheckEmail.js
+++
@@ -1,5 +0,0 @@
-export const isValidEmail = (email) => {
-    // Regular expression for validating email addresses
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-  };
